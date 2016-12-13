357d542b48053c66b37e7a04089b62c4f4ef455e
5Paisa-yog <yogesh.maurya@5paisa.com>
2016-12-13 10:54:28 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,5 +1,5 @@
 <?php
-//use FacebookAds\Object\Lead;
+use FacebookAds\Object\Lead;
 
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
@@ -9,11 +9,11 @@ if ($verify_token === 'abc123') {
 }
 
 $input = json_decode(file_get_contents('php://input'), true);
-//error_log(print_r($input, true));
+error_log(print_r($input, true));
 
-$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
-error_log(print("LeadGen Id".$leadgen_id));
-
-//$form = new Lead($leadgen_id);
+//$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
+//error_log(print("LeadGen Id".$leadgen_id));
+//
+//$form = new Lead(<LEAD_ID>);
 //$form->read();
 //error_log(print("LeadGen Details".$form));
