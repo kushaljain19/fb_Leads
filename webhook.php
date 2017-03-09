b9b73837d3685856d4cbe5a3233e2f3385de2e33
5Paisa-yog <yogesh.maurya@5paisa.com>
2017-03-09 16:51:07 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -11,8 +11,8 @@ if ($verify_token === 'abc123') {
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
 
-$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
-error_log(print("LeadGen Id".$leadgen_id));
+//$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
+//error_log(print("LeadGen Id".$leadgen_id));
 
 // $form = new Lead(<LEAD_ID>);
 // $form->read();
