bbddbe572cce7d7170c472991001b53e20157480
5Paisa-yog <yogesh.maurya@5paisa.com>
2017-03-09 16:23:37 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,17 +1,5 @@
 <?php
-include ('FacebookAds/Api.php');
-use FacebookAds\Api;
-function __autoload($class_name) {
-  require_once $class_name . '.php';
-}
-$app_id = '1244334662267045';
-$app_secret = 'd47dc67891dd5960e433dc29876f8c51';
-$access_token = '1244334662267045|Ep2fKTz7CbLpzWEwZHPopktcQfc';
-// Initialize a new Session and instanciate an Api object
-Api::init($app_id, $app_secret, $access_token);
-
-// The Api object is now available trough singleton
-$api = Api::instance();
+//use FacebookAds\Object\Lead;
 
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
@@ -20,17 +8,12 @@ if ($verify_token === 'abc123') {
   echo $challenge;
 }
 
-error_log("$challenge = ".print_r($challenge, true));
-error_log("$verify_token = ".print_r($verify_token, true));
-
-
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
 
 $leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
-error_log(print_r($leadgen_id,true));
+error_log(print("LeadGen Id".$leadgen_id));
 
-//use FacebookAds\Object\Lead;
-//$form = new Lead($leadgen_id);
-//$form->read();
-//error_log(print_r("LeadGen Details = ".$form,true));
+// $form = new Lead(<LEAD_ID>);
+// $form->read();
+// error_log(print("LeadGen Details".$form));
