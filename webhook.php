3419d930769c60ee9e7c844e8276e9f290e204ba
5Paisa-yog <yogesh.maurya@5paisa.com>
2017-03-17 12:16:04 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -32,6 +32,8 @@ curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
 $st=curl_exec($ch); 
 $result=json_decode($st,TRUE);
+error_log(print("Hello"));
+error_log(print_r($input[0]["field_data"], true));
 error_log(print_r($input["field_data"], true));
 //error_log(print_r($result,true));
 //$name = $result["field_data"][3]["values"][0];
@@ -39,4 +41,4 @@ error_log(print_r($input["field_data"], true));
 //$mobile = $result["field_data"][4]["values"][0];
 //$city = $result["field_data"][5]["values"][0];
 //error_log(print("Name = {$name}, Email = {$email}, Mobile = {$mobile}, City = {$city}"));
-error_log(print($name));
+//error_log(print($name));
