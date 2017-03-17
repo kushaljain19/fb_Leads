26dd8c8d00c98d07bdcfa0655478f41d9d961fc3
5Paisa-yog <yogesh.maurya@5paisa.com>
2017-03-17 10:51:18 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,12 +1,19 @@
 <?php 
-session_start();
-require_once __DIR__ . '/Facebook/autoload.php';
-$fb = new \Facebook\Facebook([
-  'app_id' => '1244334662267045',
-  'app_secret' => 'd47dc67891dd5960e433dc29876f8c51',
-  'default_graph_version' => 'v2.8',
-]);
-   $permissions = []; // optional
-   $helper = $fb->getRedirectLoginHelper();
-   $accessToken = $helper->getAccessToken();
-echo $accessToken;
+$url = "https://graph.facebook.com/v2.8/1289679251122612?access_token=EAARrtz2GsKUBALd8MakhGAQprYKeucW9nZC1oXbohk1pl8V3TZBP9GAN8VIBeI1NGdPCjyRWnOIGSnBcNh7DlBVGkoGvwOoRuwdmPqhcJEZBzPAJxi65l5LaVNHsOVOler89vXylk6iBahVdFTSFh0np1jHRgrDlzx7p9qZAxgZDZD";
+$headers = array("Content-type: application/json");
+
+
+ $ch = curl_init();
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ curl_setopt($ch, CURLOPT_URL, $url);
+       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
+ curl_setopt($ch, CURLOPT_COOKIEJAR,'cookie.txt');  
+ curl_setopt($ch, CURLOPT_COOKIEFILE,'cookie.txt');  
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+ curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3"); 
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+
+ $st=curl_exec($ch); 
+ $result=json_decode($st,TRUE);
+echo $result;
