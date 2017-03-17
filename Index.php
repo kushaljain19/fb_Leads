053e3d11a9799c084db890fbed6b1dd1bd3dc1e4
5Paisa-yog <yogesh.maurya@5paisa.com>
2017-03-17 15:52:00 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -13,10 +13,26 @@ curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
 $st=curl_exec($ch); 
 $result=json_decode($st,TRUE);
-echo print_r($result,true);
-echo "<br/>";
+//echo print_r($result,true);
 //$email='', $name='', $phone_number='',$city='',$description=''; 
 //echo print_r($result["field_data"][0]["values"], true);
+$FormDetailUrl = "https://graph.facebook.com/v2.8/1188529894570882?access_token=EAARrtz2GsKUBALd8MakhGAQprYKeucW9nZC1oXbohk1pl8V3TZBP9GAN8VIBeI1NGdPCjyRWnOIGSnBcNh7DlBVGkoGvwOoRuwdmPqhcJEZBzPAJxi65l5LaVNHsOVOler89vXylk6iBahVdFTSFh0np1jHRgrDlzx7p9qZAxgZDZD";
+$FormHeaders = array("Content-type: application/json");
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, $FormHeaders);
+curl_setopt($ch, CURLOPT_URL, $FormDetailUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
+curl_setopt($ch, CURLOPT_COOKIEJAR,'cookie.txt');  
+curl_setopt($ch, CURLOPT_COOKIEFILE,'cookie.txt');  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3"); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+$st=curl_exec($ch); 
+$FormDetails=json_decode($st,TRUE);
+echo print_r($FormDetails,true);
+
+
 $FieldData = $result["field_data"];
 foreach ( $FieldData as $key=>$val ){
    print "$key = ".print_r($val,true)."<br>";
@@ -43,4 +59,4 @@ foreach ( $FieldData as $key=>$val ){
 //print "$city <br/>";
 //print "$description <br/>";
 $strZohoUrl = "https://crm.zoho.com/crm/private/xml/Leads/insertRecords?scope=crmapi&newFormat=1&version=2&duplicateCheck=2&wfTrigger=true&authtoken=b01ef977ae5d658b4368ebe181cf5bd9&xmlData=<Leads><row no='1'><FL val='Last Name'>".$name."</FL><FL val='Email'>".$email."</FL><FL val='City'>".$city."</FL><FL val='Lead Source'>Facebook Lead Ad</FL><FL val='Mobile'>".$phone_number."</FL><FL val='Description'>".$description."</FL></row></Leads>";
-print "$strZohoUrl <br/>";
+print "$strZohoUrl";
