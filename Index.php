259757477aee9d97d22d30a8524413c0b881741f
5Paisa-yog <yogesh.maurya@5paisa.com>
2017-03-17 12:48:33 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,19 +1,17 @@
 <?php 
-$url = "https://graph.facebook.com/v2.8/1289679251122612?access_token=EAARrtz2GsKUBALd8MakhGAQprYKeucW9nZC1oXbohk1pl8V3TZBP9GAN8VIBeI1NGdPCjyRWnOIGSnBcNh7DlBVGkoGvwOoRuwdmPqhcJEZBzPAJxi65l5LaVNHsOVOler89vXylk6iBahVdFTSFh0np1jHRgrDlzx7p9qZAxgZDZD";
+$url = "https://graph.facebook.com/v2.8/245520369242837?access_token=EAARrtz2GsKUBALd8MakhGAQprYKeucW9nZC1oXbohk1pl8V3TZBP9GAN8VIBeI1NGdPCjyRWnOIGSnBcNh7DlBVGkoGvwOoRuwdmPqhcJEZBzPAJxi65l5LaVNHsOVOler89vXylk6iBahVdFTSFh0np1jHRgrDlzx7p9qZAxgZDZD";
 $headers = array("Content-type: application/json");
-
-
- $ch = curl_init();
- curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
- curl_setopt($ch, CURLOPT_URL, $url);
-       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
- curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
- curl_setopt($ch, CURLOPT_COOKIEJAR,'cookie.txt');  
- curl_setopt($ch, CURLOPT_COOKIEFILE,'cookie.txt');  
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
- curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3"); 
- curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
-
- $st=curl_exec($ch); 
- $result=json_decode($st,TRUE);
-echo print_r($result,true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
+curl_setopt($ch, CURLOPT_COOKIEJAR,'cookie.txt');  
+curl_setopt($ch, CURLOPT_COOKIEFILE,'cookie.txt');  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3"); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+$st=curl_exec($ch); 
+$result=json_decode($st,TRUE);
+print_r($result,true);
+print_r($result["field_data"][0]["values"], true);
