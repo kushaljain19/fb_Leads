03e1be5ad66fe9f4de2beb18ab23f0fb5e72fed3
5Paisa-yog <yogesh.maurya@5paisa.com>
2017-03-17 10:55:52 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -11,9 +11,25 @@ if ($verify_token === 'abc123') {
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
 
-//$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
+$leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
 //error_log(print("LeadGen Id".$leadgen_id));
 
 // $form = new Lead(<LEAD_ID>);
 // $form->read();
 // error_log(print("LeadGen Details".$form));
+
+$url = "https://graph.facebook.com/v2.8/{$leadgen_id}?access_token=EAARrtz2GsKUBALd8MakhGAQprYKeucW9nZC1oXbohk1pl8V3TZBP9GAN8VIBeI1NGdPCjyRWnOIGSnBcNh7DlBVGkoGvwOoRuwdmPqhcJEZBzPAJxi65l5LaVNHsOVOler89vXylk6iBahVdFTSFh0np1jHRgrDlzx7p9qZAxgZDZD";
+$headers = array("Content-type: application/json");
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
+curl_setopt($ch, CURLOPT_COOKIEJAR,'cookie.txt');  
+curl_setopt($ch, CURLOPT_COOKIEFILE,'cookie.txt');  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3"); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+$st=curl_exec($ch); 
+$result=json_decode($st,TRUE);
+error_log(print_r($result,true));
