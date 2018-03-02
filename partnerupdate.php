47e04d223fb289f9efb26676efbaa8d6bd0e81fc
5Paisa-yog <yogesh.maurya@5paisa.com>
2018-03-02 17:09:37 +0530

    Update partnerupdate.php

--- partnerupdate.php
+++ partnerupdate.php
@@ -4,9 +4,20 @@ $movies = array(
   array("user_Id" => "1699841000340851001","reporting_id" => "1699841000334764098"),
   array("user_Id" => "1699841000340851041","reporting_id" => "1699841000311564490")
 );
+$userd_id_param="user_Id";
+$reporting_id_param="reporting_id";
 foreach ( $movies as $movie ) {
   foreach ( $movie as $key => $value ) {
     error_log ("<dt>$key</dt><dd>$value</dd>");
+    if(strpos($key, $userd_id_param)!== false){
+      $recordId = $value;
+    }
+    if(strpos($key, $reporting_id_param)!== false){
+      $reporting_id = $value;
+    }
+  }
+  error_log ($recordId);
+  error_log ($reporting_id); 
   }
 }
 ?>
