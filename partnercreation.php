aa564baac289208afcd8a01e6dc3ef647fa85b9e
5Paisa-yog <yogesh.maurya@5paisa.com>
2018-03-02 23:33:04 +0530

    Update partnercreation.php

--- partnercreation.php
+++ partnercreation.php
@@ -19,5 +19,30 @@
                 }
               }
             }
-          
+$access_token = "1000.321c4dc22d30b09b4012db1451de7e75.7e64542eb893772940061113b86b67a3";
+$zohocrmurl = "https://one.zoho.com/api/v1/orgs/655853087/appaccounts/62042541/members";
+$accesstokenparam = "Zoho-oauthtoken"." ".$access_token;
+$headers = array("Authorization: $accesstokenparam");
+$content_type = "application/json";
+//$headers->Content-type = $content_type;
+//$headers->Authorization = $accesstokenparam;
+error_log(json_encode($headers));
+$ch = curl_init();  
+$param4 = '{"members" : {"role_id" : "1699841000000026008","profile_id" : "1699841000336610025","zaaid" : "62047589","zuid" :';
+$param5 = '}}';
+$param1 = '{"users" : {"first_name" : "5P Partner","last_name" :';
+$param2 = ',"emails" : [{"email_id" : ';
+$param3 = ',}],"generate_password" : true,}}';
+$json = $param1.$last_name.$param2.$email_id.$param3;
+$json2 = $param4.$users_id.$param5;
+error_log($json2);
+$result = json_decode ($json);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS,$json2);
+curl_setopt($ch, CURLOPT_URL, $zohocrmurl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+$st=curl_exec($ch);
+error_log("first response");
+error_log($st);          
 ?>
