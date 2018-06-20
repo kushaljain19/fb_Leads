5de71d6a9bbdd18d1581f4546c10cf138e76b2ef
5Paisa-yog <yogesh.maurya@5paisa.com>
2018-06-20 16:13:08 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -26,7 +26,8 @@ error_log($leadgen_id);
 error_log($form_id);
 
 //Fetch Lead Data using graph api while accesing the lead id as a node and access token as a parameter(This access token never expires)
-$url = "https://graph.facebook.com/v2.8/{$leadgen_id}?access_token=EAADmDVFtzhgBAIgCQ1h5xRKQiRzgi4pcbKzivZCJRGj8xq1dEioMMy2vSE6ZCZAQyEigexqDlknd6Pr0H7xjOy1pyXQ6d6eI1leUq9125autpLpa3aRjQBEIDzpqU2A12ZBVIIe3jdaTly9F5rFMjahUP5zcOdQZD";
+$url = "https://graph.facebook.com/v2.8/{$leadgen_id}?access_token=EAADmDVFtzhgBAHSp3kQWgYo4rhva64P3jQK0xh7rnFDlz3AOhnZBMzkU3qcxFuZBTb9Lo3e866xXyE1N5uoqhyQ2tWw7kZBZAZCOZAfrmI6ewYfcsbNZApFX7GjaM1dda7cYLLeg5hCmvnMnJ1fVJYs3LykTxV0aZBIZD";
+//EAADmDVFtzhgBAIgCQ1h5xRKQiRzgi4pcbKzivZCJRGj8xq1dEioMMy2vSE6ZCZAQyEigexqDlknd6Pr0H7xjOy1pyXQ6d6eI1leUq9125autpLpa3aRjQBEIDzpqU2A12ZBVIIe3jdaTly9F5rFMjahUP5zcOdQZD";
 //EAADmDVFtzhgBANi9ePToQFPL0CNrzDGg24SDtw1YdyqfITXNLtyL4jur6zPN6lKCIeyTu5vY1o40YYRzZCAiLCxKHqkZCm5VHb4noM8wZB51nx92ypfj6LdEb0WyVt9CdD5v4dGILK0C3xydUWlH0Fm1LGI66IZD";
 //EAADmDVFtzhgBANi9ePToQFPL0CNrzDGg24SDtw1YdyqfITXNLtyL4jur6zPN6lKCIeyTu5vY1o40YYRzZCAiLCxKHqkZCm5VHb4noM8wZB51nx92ypfj6LdEb0WyVt9CdD5v4dGILK0C3xydUWlH0Fm1LGI66IZD
 $headers = array("Content-type: application/json");
@@ -182,7 +183,8 @@ foreach ( $FieldData as $key=>$val ){
 }  
 
 //Fetching Form Name using Form Id to use as a campaign name
-$FormDetailUrl = "https://graph.facebook.com/v2.8/{$form_id}?access_token=EAADmDVFtzhgBANi9ePToQFPL0CNrzDGg24SDtw1YdyqfITXNLtyL4jur6zPN6lKCIeyTu5vY1o40YYRzZCAiLCxKHqkZCm5VHb4noM8wZB51nx92ypfj6LdEb0WyVt9CdD5v4dGILK0C3xydUWlH0Fm1LGI66IZD";
+$FormDetailUrl = "https://graph.facebook.com/v2.8/{$form_id}?access_token=EAADmDVFtzhgBAHSp3kQWgYo4rhva64P3jQK0xh7rnFDlz3AOhnZBMzkU3qcxFuZBTb9Lo3e866xXyE1N5uoqhyQ2tWw7kZBZAZCOZAfrmI6ewYfcsbNZApFX7GjaM1dda7cYLLeg5hCmvnMnJ1fVJYs3LykTxV0aZBIZD";
+//EAADmDVFtzhgBANi9ePToQFPL0CNrzDGg24SDtw1YdyqfITXNLtyL4jur6zPN6lKCIeyTu5vY1o40YYRzZCAiLCxKHqkZCm5VHb4noM8wZB51nx92ypfj6LdEb0WyVt9CdD5v4dGILK0C3xydUWlH0Fm1LGI66IZD";
 //EAADmDVFtzhgBANi9ePToQFPL0CNrzDGg24SDtw1YdyqfITXNLtyL4jur6zPN6lKCIeyTu5vY1o40YYRzZCAiLCxKHqkZCm5VHb4noM8wZB51nx92ypfj6LdEb0WyVt9CdD5v4dGILK0C3xydUWlH0Fm1LGI66IZD
 $FormHeaders = array("Content-type: application/json");
 $ch = curl_init();
